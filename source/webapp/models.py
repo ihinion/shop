cd3fd414b040bbaf1c6c5c6c8669a19cd9b96250
ilia <jamiashvili@gmail.com>
2020-07-31 11:33:14 +0600

    Added validators for amount and price fields in model Product, migrated database

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -16,8 +17,8 @@ class Product(models.Model):
     description = models.TextField(max_length=2000, verbose_name='Description', null=True, blank=True)
     category = models.CharField(max_length=20, verbose_name='Category',
                                 choices=CATEGORY_CHOICES, default=DEFAULT_CATEGORY)
-    amount = models.IntegerField(verbose_name='Amount')
-    price = models.DecimalField(verbose_name='Price', max_digits=7, decimal_places=2)
+    amount = models.IntegerField(verbose_name='Amount', validators=[MinValueValidator(0)])
+    price = models.DecimalField(verbose_name='Price', max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
 
     def __str__(self):
         return f'{self.name} - {self.amount}'
