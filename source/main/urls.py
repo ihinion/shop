d8c1542cc76f57d7a494bf5ae401915eae4551a2
ilia <jamiashvili@gmail.com>
2020-08-02 18:46:27 +0600

    Created add.html, add_view, path in urls.py, regustered widget_tweaks in settings.py, added links in index.html

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view, product_view
+from webapp.views import index_view, product_view, add_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
-    path('product/<pk>/', product_view, name='product')
+    path('product/<pk>/', product_view, name='product'),
+    path('add/', add_view, name='add')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,12 +1,26 @@
 from webapp.models import Product
 from django.shortcuts import redirect, get_object_or_404, render
+from webapp.forms import ProductForm
 
 
 def index_view(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('category', 'name')
     return render(request, 'index.html', {'products': products})
 
 
 def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'product.html', {'product': product})
+
+
+def add_view(request):
+    if request.method == 'GET':
+        form = ProductForm()
+        return render(request, 'add.html', {'form': form})
+    elif request.method == 'POST':
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product = Product.objects.create(**form.cleaned_data)
+            return redirect('product', pk=product.pk)
+        else:
+            return render(request, 'add.html', {'form': form})
